d677e63f51d1e947d024696291cfcbbe71f428d9
Oaitse Lepodise <75835339+olepodisegithub@users.noreply.github.com>
2023-01-12 05:44:43 +0200

    3,4 and 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = 0;
+        int guess = 0;
+        Boolean guessed = false;
+        int guessNumber = 0;
+        Boolean play = true;
+
+        while (play == true)
+        {
+            guessed = false;
+            Console.WriteLine("");
+
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1, 100);
+            Console.WriteLine("Margic number is between 1 and 100");
+
+            while (guessed == false)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Go lower!");
+                    guessNumber = guessNumber + 1;
+                }
+                else if (guess == magicNumber)
+                {
+                    Console.WriteLine($"Correct!. It took you {guessNumber} try(es) to get it");
+                    guessed = true;
+                    Console.Write("Do you want to proceed playing? (Enter yes / no): ");
+                    if (Console.ReadLine().ToLower() == "yes")
+                    {
+                        play = true;
+                    }
+                    else
+                    {
+                        play = false;
+                    }
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Go Higher");
+                    guessNumber = guessNumber + 1;
+                }
+            }
+            Console.WriteLine("");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Boolean add = true;
+        List<int> numbers = new List<int>();
+        int num = 0;
+        int sum = 0;
+        int largest = 0;
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Add numbers. enter 0 if you are done!");
+
+        while (add == true)
+        {
+            Console.Write("Enter number? ");
+            num = int.Parse(Console.ReadLine());
+            if (num == 0)
+            {
+                add = false;
+            }
+            else
+            {
+                numbers.Add(num);
+            }
+        }
+
+        foreach (int n in numbers)
+        {
+            sum = sum + n;
+            if (n > largest)
+            {
+                largest = n;
+            }
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"Average is: {sum / numbers.Count}");
+        Console.WriteLine($"The larget is: {largest}");
     }
 }
